b37204d55e2e905d1c29a8d6ae6e4b6421f76801
Wibnu Dickson <wibnudickson@gmail.com>
2021-07-21 20:49:32 +0200

    Create app_uno.py

---
+++ app_uno.py
@@ -0,0 +1,24 @@
+import json
+from difflib import get_close_matches
+data = json.load(open("data.json"))
+
+def translate(w):
+    w = w.lower()
+    if w in data:
+       return data[w]
+    elif len(get_close_matches(w,data.keys())) > 0:
+        yn=input("Did you mean %s instead? Enter Y if yes, or No if no:" % get_close_matches(w, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesnt exist"
+        else:
+            return "We didnt uncerstand your query"
+    else:
+        return "The word does not exist."
+
+        
+
+word = input("Enter word :")
+
+print(translate(word))
